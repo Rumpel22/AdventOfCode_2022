1be918d42f67ed985f51521c6253bfffc46666f0
Pascal Wey <pascal@pfft.ch>
2022-12-08 20:36:07 +0000

    Cleanup day 1

--- src/bin/day_01/main.rs
+++ src/bin/day_01/main.rs
@@ -1,26 +1,21 @@
-use std::{fs, path::Path};
+use itertools::Itertools;
 
 fn main() {
-    let path = Path::new("src/bin/day_01/input.txt");
-    match fs::read_to_string(path) {
-        Ok(input) => {
-            let mut elves = input
-                .split("\n\n")
-                .map(|grp| {
-                    grp.split('\n')
-                        .map(|line| line.parse::<i32>().unwrap())
-                        .sum::<i32>()
-                })
-                .collect::<Vec<_>>();
-            elves.sort();
-            elves.reverse();
-            let max_value = elves.first().unwrap();
+    let input = include_str!("input.txt");
 
-            let top_3_elves = elves.iter().take(3).sum::<i32>();
-            println!("Solution 1: {max_value}, solution 2: {top_3_elves}");
-        }
-        Err(err) => {
-            println!("{err}")
-        }
-    }
+    let (solution1, solution2) = input
+        .split("\n\n")
+        .map(|group| {
+            group
+                .split('\n')
+                .map(|line| line.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .sorted_by_key(|&v| std::cmp::Reverse(v))
+        .take(3)
+        .fold((u32::MIN, u32::default()), |(max, sum), value| {
+            (max.max(value), sum + value)
+        });
+
+    println!("Solution 1: {solution1}, solution 2: {solution2}");
 }
