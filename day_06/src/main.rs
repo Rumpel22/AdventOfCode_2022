14543c82d338a1794768efc4bdcc1eb7fbfa6f42
Pascal Wey <pascal@pfft.ch>
2022-12-19 21:55:27 +0000

    Better solution day 6

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,20 +1,18 @@
+use itertools::Itertools;
+
+fn solution(length: usize, input: &str) -> usize {
+    input
+        .as_bytes()
+        .windows(length)
+        .position(|x| x.iter().unique().count() == x.len())
+        .unwrap()
+        + length
+}
+
 fn main() {
     let input = include_str!("../data/input.txt");
 
-    let solution_1 = input
-        .as_bytes()
-        .windows(4)
-        .enumerate()
-        .find(|(_, window)| !(1..window.len()).any(|i| window[i..].contains(&window[i - 1])))
-        .map(|(index, _)| index + 4)
-        .unwrap();
-
-    let solution_2 = input
-        .as_bytes()
-        .windows(14)
-        .enumerate()
-        .find(|(_, window)| !(1..window.len()).any(|i| window[i..].contains(&window[i - 1])))
-        .map(|(index, _)| index + 14)
-        .unwrap();
+    let solution_1 = solution(4, input);
+    let solution_2 = solution(14, input);
     println!("Solution 1: {solution_1}, solution 2: {solution_2}");
 }
