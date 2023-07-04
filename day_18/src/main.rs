2f51014f74eade86f2a05f172c5a7f39a37e5b65
Pascal Wey <566223+Rumpel22@users.noreply.github.com>
2023-07-04 19:57:51 +0000

    Solution day 18, part 2

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -1,5 +1,6 @@
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
+#[derive(Eq, PartialEq, Hash, Clone, Copy)]
 struct Cube {
     x: i32,
     y: i32,
@@ -19,20 +20,157 @@ impl FromStr for Cube {
 }
 
 impl Cube {
+    fn new(x: i32, y: i32, z: i32) -> Self {
+        Self { x, y, z }
+    }
+
     fn connected_to(&self, cube: &Cube) -> bool {
         (self.x - cube.x).abs() + (self.y - cube.y).abs() + (self.z - cube.z).abs() == 1
     }
+
+    fn above(&self) -> Self {
+        Self {
+            z: self.z - 1,
+            ..*self
+        }
+    }
+    fn beneath(&self) -> Self {
+        Self {
+            z: self.z + 1,
+            ..*self
+        }
+    }
+    fn left(&self) -> Self {
+        Self {
+            y: self.y - 1,
+            ..*self
+        }
+    }
+    fn right(&self) -> Self {
+        Self {
+            y: self.y + 1,
+            ..*self
+        }
+    }
+    fn front(&self) -> Self {
+        Self {
+            x: self.x + 1,
+            ..*self
+        }
+    }
+    fn back(&self) -> Self {
+        Self {
+            x: self.x - 1,
+            ..*self
+        }
+    }
+
+    fn neighbors(&self) -> CubeNeighbors {
+        CubeNeighbors {
+            cube: *self,
+            index: 0,
+        }
+    }
 }
 
-fn main() {
-    let input = include_str!("../data/input.txt");
+struct CubeNeighbors {
+    cube: Cube,
+    index: usize,
+}
 
-    let cubes = input
-        .lines()
-        .map(|line| line.parse::<Cube>().unwrap())
-        .collect::<Vec<_>>();
+impl Iterator for CubeNeighbors {
+    type Item = Cube;
 
-    let covered_sites = cubes
+    fn next(&mut self) -> Option<Self::Item> {
+        let cube = match self.index {
+            0 => self.cube.above(),
+            1 => self.cube.beneath(),
+            2 => self.cube.left(),
+            3 => self.cube.right(),
+            4 => self.cube.front(),
+            5 => self.cube.back(),
+            _ => return None,
+        };
+        self.index += 1;
+        Some(cube)
+    }
+}
+
+#[derive(PartialEq)]
+enum Content {
+    Lava,
+    Water,
+    Unknown,
+}
+struct Vessel {
+    map: HashMap<Cube, Content>,
+    first: Cube,
+}
+
+impl Vessel {
+    fn new_for_lava(cubes: &[Cube]) -> Self {
+        // Allow some water to flow around the lava
+        let x_min = cubes.iter().min_by(|a, b| a.x.cmp(&b.x)).unwrap().x - 1;
+        let y_min = cubes.iter().min_by(|a, b| a.y.cmp(&b.y)).unwrap().y - 1;
+        let z_min = cubes.iter().min_by(|a, b| a.z.cmp(&b.z)).unwrap().z - 1;
+        let x_max = cubes.iter().max_by(|a, b| a.x.cmp(&b.x)).unwrap().x + 1;
+        let y_max = cubes.iter().max_by(|a, b| a.y.cmp(&b.y)).unwrap().y + 1;
+        let z_max = cubes.iter().max_by(|a, b| a.z.cmp(&b.z)).unwrap().z + 1;
+
+        // This one is guaranteed to be empty and will be filled with water
+        let first = Cube::new(x_min, y_min, z_min);
+
+        let x_dim = x_min..=x_max;
+        let y_dim = y_min..=y_max;
+        let z_dim = z_min..=z_max;
+
+        // Create empty map with all cubes set to unknown
+        let mut map = x_dim
+            .flat_map(|x| y_dim.clone().map(move |y| (x, y)))
+            .flat_map(|(x, y)| z_dim.clone().map(move |z| (x, y, z)))
+            .map(|(x, y, z)| (Cube::new(x, y, z), Content::Unknown))
+            .collect::<HashMap<_, _>>();
+
+        // Insert lava
+        map.extend(cubes.iter().map(|cube| (*cube, Content::Lava)));
+
+        Self { map, first }
+    }
+
+    fn fill_water(&mut self) {
+        let mut candidates = vec![self.first];
+
+        while let Some(candidate) = candidates.pop() {
+            self.map
+                .entry(candidate)
+                .and_modify(|x| *x = Content::Water);
+
+            for neighbor in candidate.neighbors() {
+                if let Some((_, content)) = self.map.get_key_value(&neighbor) {
+                    if content == &Content::Unknown {
+                        candidates.push(neighbor);
+                    }
+                }
+            }
+        }
+    }
+
+    fn air(&self) -> Vec<Cube> {
+        self.map
+            .iter()
+            .filter_map(|(cube, content)| {
+                if content == &Content::Unknown {
+                    Some(*cube)
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+}
+
+fn get_total_surface(cubes: &[Cube]) -> usize {
+    let sites = cubes
         .iter()
         .map(|cube| {
             cubes
@@ -42,5 +180,26 @@ fn main() {
         })
         .sum::<usize>();
     let total_sites = cubes.len() * 6;
-    println!("{}", total_sites - covered_sites);
+    total_sites - sites
+}
+
+fn main() {
+    let input = include_str!("../data/input.txt");
+
+    let lava_cubes = input
+        .lines()
+        .map(|line| line.parse::<Cube>().unwrap())
+        .collect::<Vec<_>>();
+
+    let total_surface = get_total_surface(&lava_cubes);
+    println!("Total surface of the droplet is {}.", total_surface);
+
+    let mut vessel = Vessel::new_for_lava(&lava_cubes);
+    vessel.fill_water();
+
+    let air_cubes = vessel.air();
+    let air_surface = get_total_surface(&air_cubes);
+
+    let outer_surface = total_surface - air_surface;
+    println!("The total outer surface is {}.", outer_surface);
 }
